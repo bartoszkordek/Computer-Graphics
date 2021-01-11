59b06d7f4d1af20a75c003d36d70eb43c344a8c7
bartoszkordek <bartosz.kordek10@gmail.com>
2021-01-11 22:49:30 +0100

    project 7 Simple WebGL Triangle

---
+++ lab7/Triangle/triangle.js
@@ -0,0 +1,31 @@
+var gl;
+var points;
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" );
+ }
+var vertices = new Float32Array([-1, -1, 1, -1, 0, 0, -0.5, 0, 0.5, 0, 0, 0.5, -0.25, 0.5, 0.25, 0.5, 0, 0.75]);
+ //  Configure WebGL
+gl.viewport( 0, 0, canvas.width, canvas.height );
+gl.clearColor( 0.5, 0.2, 0.1, 0.8 );
+//  load shaders 
+   var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+   	gl.useProgram( program );
+ 
+   // Initiate buffer and load data to GPU
+   var bufferId = gl.createBuffer();
+   gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+   gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+// Associate shader variables with buffer data and draw  
+ var vP = gl.getAttribLocation( program, "vPosition" );
+ gl.vertexAttribPointer( vP, 2, gl.FLOAT, false, 0, 0 );
+ gl.enableVertexAttribArray( vP ); 
+ render();
+};
+function render() 
+{
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 9 );
+}
